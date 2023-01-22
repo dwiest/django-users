46a50bc8f672993f9f8e7d9e29b042f66bd9b334
dwiest <damian@wiest.world>
2023-01-22 22:08:33 +0000

    Render a section for the user's MfaModel object on the admin page.

--- dwiest/django/users/admin.py
+++ dwiest/django/users/admin.py
@@ -3,15 +3,21 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 from .models import ActivationId
+from .mfa.models import MfaModel
 
 class ActivationIdInline(admin.StackedInline):
   model = ActivationId
   can_delete = False
   verbose_name_plural = 'activationid'
 
+class MfaInline(admin.StackedInline):
+  model = MfaModel
+  can_delete = False
+  verbose_name_plural = 'multi-factor authentication'
+
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
-  inlines = (ActivationIdInline,)
+  inlines = (ActivationIdInline,MfaInline)
 
 # Re-register UserAdmin
 admin.site.unregister(User)
