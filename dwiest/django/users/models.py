110baf1aeadf64d7f74b2a1a6bbb58d9e14b763c
dwiest <dw9494@gmail.com>
2023-01-27 23:32:04 +0000

    Added created_iat DateTimeField to ActivationId.

--- dwiest/django/users/models.py
+++ dwiest/django/users/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class ActivationId(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   value = models.CharField(max_length=36)
+  created_at = models.DateTimeField(auto_now_add=True)
