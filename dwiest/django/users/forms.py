cb3f41548084fe3de7637d977bf4b6d4c3303abf
dwiest <damian@wiest.world>
2023-01-27 21:14:45 +0000

    Changed email field label for SendResetEmailForm.
    Added PasswordResetConfirmForm class.
    
    Removed update_failed from PasswordChangeForm's error list, no longer
    used.
    
    Removed some commented out lines and print statements.
    
    Removed clean_activation_id from the PasswordChangeForm class, this is
    now handled in PasswordChangeView.

--- dwiest/django/users/forms.py
+++ dwiest/django/users/forms.py
@@ -33,7 +33,7 @@ class RegistrationForm(UserCreationForm):
 
 
 class SendPasswordResetForm(forms.Form):
-  email = forms.EmailField(label='email',initial='',max_length=50)
+  email = forms.EmailField(label='Email',initial='',max_length=50)
 
   def sendPasswordResetEmail(self):
     email = self.cleaned_data.get('email')
@@ -55,7 +55,15 @@ class SendPasswordResetForm(forms.Form):
       send_email(settings.EMAIL_SENDER, recipients, email_message.as_string(), settings.SMTP_SERVER, smtp_server_login=settings.EMAIL_SENDER, smtp_server_password=settings.SMTP_SERVER_PASSWORD, proxy_server=settings.PROXY_SERVER, proxy_port=settings.PROXY_PORT)
 
 
-class PasswordChangeForm(authForms.PasswordChangeForm):
+
+class PasswordResetConfirmForm(authForms.PasswordChangeForm):
+
+  activation_id = forms.CharField(
+    label=_('Activation Id'),
+    required=False,
+    widget=forms.HiddenInput(),
+    )
+
   mfa_token = forms.CharField(
     label=_("MFA Token"),
     max_length=6,
@@ -63,30 +71,43 @@ class PasswordChangeForm(authForms.PasswordChangeForm):
     required=False,
     widget=NonstickyTextInput(attrs={'size': 6}))
 
-  authForms.PasswordChangeForm.base_fields['old_password'].label = 'Password'
+  old_password = None
+
   authForms.PasswordChangeForm.base_fields['new_password1'].label = 'New Password'
   authForms.PasswordChangeForm.base_fields['new_password2'].label = 'Confirm Password'
 
   authForms.PasswordChangeForm.error_messages.update({
     'password_mismatch':
       _("The two password fields didn't match."),
-    'update_failed':
-      _("Your password could not be updated."),
-    'invalid_user':
-      _("Your password could not be updated."),
     'invalid_mfa_token': _(
       "The MFA token you entered is not correct."
     ),
     'replayed_mfa_token': _(
       "The MFA token you entered has already been used.  Please wait and enter the next value shown in your authenticator app."
     ),
+    'activation_id_invalid': _(
+      "The activation id is invalid."
+    ),
+    'activation_id_missing': _(
+      "An activation id was not provided."
+    ),
   })
 
-  activation_id = forms.CharField(label='activation_id',initial='',required=False, max_length=36)
-
-#  def __init__(self, user, *args, **kwargs):
-#    self.user = user
-#    super(SetPasswordForm, self).__init__(*args, **kwargs)
+  def clean_activation_id(self):
+    activation_id = self.cleaned_data.get("activation_id")
+    if activation_id and activation_id != '':
+      try:
+        user_activation_id = ActivationId.objects.get(value=activation_id)
+        self.user = User.objects.get(id=user_activation_id.user_id, is_active=True)
+      except ObjectDoesNotExist:
+        raise forms.ValidationError(
+          self.error_messages['activation_id_invalid'],
+          code='activation_id_invalid',)
+      return activation_id
+    else:
+      raise forms.ValidationError(
+        self.error_messages['activation_id_missing'],
+        code='activation_id_missing',)
 
   def clean_mfa_token(self):
     mfa_token = self.cleaned_data.get('mfa_token')
@@ -101,18 +122,14 @@ class PasswordChangeForm(authForms.PasswordChangeForm):
         totp = pyotp.TOTP(user_mfa.secret_key)
 
         if mfa_token != totp.now():
-          print("invalid_mfa_token")
           raise self.get_invalid_mfa_token_error()
         elif mfa_token == user_mfa.last_value:
-          print("replayed_mfa_token")
           raise self.get_replayed_mfa_token_error()
         else:
-          print("valid_mfa_token")
           user_mfa.last_value = mfa_token
           user_mfa.save()
 
       except ObjectDoesNotExist:
-          print("No MFA object for user")
           raise self.get_invalid_mfa_token_error()
     return mfa_token
 
@@ -128,17 +145,77 @@ class PasswordChangeForm(authForms.PasswordChangeForm):
       code='replayed_mfa_token',
     )
 
-  def clean_activation_id(self):
-    activation_id = self.cleaned_data.get("activation_id")
-    if activation_id and activation_id != '':
+def activation_id_is_valid(activation_id):
+  if activation_id is not None:
+    try:
+      if ActivationId.objects.get(value=activation_id):
+        return True
+    except ObjectDoesNotExist:
+      return False
+  return False
+
+
+class PasswordChangeForm(authForms.PasswordChangeForm):
+  mfa_token = forms.CharField(
+    label=_("MFA Token"),
+    max_length=6,
+    min_length=6,
+    required=False,
+    widget=NonstickyTextInput(attrs={'size': 6}))
+
+  authForms.PasswordChangeForm.base_fields['old_password'].label = 'Password'
+  authForms.PasswordChangeForm.base_fields['new_password1'].label = 'New Password'
+  authForms.PasswordChangeForm.base_fields['new_password2'].label = 'Confirm Password'
+
+  authForms.PasswordChangeForm.error_messages.update({
+    'password_mismatch':
+      _("The two password fields didn't match."),
+    'invalid_user':
+      _("Your password could not be updated."),
+    'invalid_mfa_token': _(
+      "The MFA token you entered is not correct."
+    ),
+    'replayed_mfa_token': _(
+      "The MFA token you entered has already been used.  Please wait and enter the next value shown in your authenticator app."
+    ),
+  })
+
+
+  def clean_mfa_token(self):
+    mfa_token = self.cleaned_data.get('mfa_token')
+
+    if hasattr(settings, 'MFA_ACCEPT_ANY_VALUE') and settings.MFA_ACCEPT_ANY_VALUE:
+      print("!WARNING! MFA accepting any value")
+      mfa_token = None
+
+    if mfa_token != None:
       try:
-        user_activation_id = ActivationId.objects.get(value=activation_id)
-        self.user = User.objects.get(id=user_activation_id.user_id, is_active=True)
+        user_mfa = MfaModel.objects.get(user_id=self.user.id)
+        totp = pyotp.TOTP(user_mfa.secret_key)
+
+        if mfa_token != totp.now():
+          raise self.get_invalid_mfa_token_error()
+        elif mfa_token == user_mfa.last_value:
+          raise self.get_replayed_mfa_token_error()
+        else:
+          user_mfa.last_value = mfa_token
+          user_mfa.save()
+
       except ObjectDoesNotExist:
-        raise forms.ValidationError(
-          self.error_messages['update_failed'],
-          code='update_failed',)
-      return activation_id
+          raise self.get_invalid_mfa_token_error()
+    return mfa_token
+
+  def get_invalid_mfa_token_error(self):
+    return ValidationError(
+      self.error_messages['invalid_mfa_token'],
+      code='invalid_mfa_token',
+    )
+
+  def get_replayed_mfa_token_error(self):
+    return ValidationError(
+      self.error_messages['replayed_mfa_token'],
+      code='replayed_mfa_token',
+    )
 
   def save(self, commit=True):
     if settings.SEND_EMAIL:
