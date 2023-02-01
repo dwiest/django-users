ed95826b6254657785981f692c2b1694f79fa55a
dwiest <dw9494@gmail.com>
2023-02-01 19:54:56 +0000

    Use dwiest.django.users.conf.settings instead of django.conf.settings.

--- dwiest/django/users/urls.py
+++ dwiest/django/users/urls.py
@@ -12,12 +12,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 import dwiest.django.users.mfa
 from dwiest.django.auth.views import LoginView, PasswordChangeView
+from dwiest.django.users.conf import settings
 from .views import *
 
 urlpatterns = [
