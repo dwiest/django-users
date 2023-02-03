0488c0a9638eb21619ecdee50383e6923e3d4eb4
dwiest <dw9494@gmail.com>
2023-02-03 20:44:03 +0000

    Moved callbacks here from dwiest.django.users.email.

--- dwiest/django/users/mfa/signals.py
+++ dwiest/django/users/mfa/signals.py
@@ -1,4 +1,17 @@
 from django.dispatch import Signal
+from ..email import *
 
 mfa_enabled = Signal()
 mfa_disabled = Signal()
+
+@receiver(mfa_disabled)
+def mfa_disabled_callback(sender, **kwargs):
+  recipients = [kwargs['request'].user.email]
+  msg = generate_mfa_disabled_email(recipients)
+  send_email(recipients, msg.as_string())
+
+@receiver(mfa_enabled)
+def mfa_enabled_callback(sender, **kwargs):
+  recipients = [kwargs['request'].user.email]
+  msg = generate_mfa_enabled_email(recipients)
+  send_email(recipients, msg.as_string())
