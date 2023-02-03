21932646fe22f78cce3fa3efc2d3a2e497510626
dwiest <dw9494@gmail.com>
2023-02-03 21:08:01 +0000

    Use relative path for import of conf.settings.
    Removed unused imports.
    
    Moved mfa callbacks to mfa/signals.py.
    
    generate_password_change_email -> generate_password_changed_email
    
    Put keyword arguments on separate lines for send_email().

--- dwiest/django/users/email.py
+++ dwiest/django/users/email.py
@@ -1,23 +1,10 @@
 from django.dispatch import receiver
 from django.shortcuts import render
 from django.template.loader import get_template
-from dwiest.django.users.conf import settings
+from .conf import settings
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
-from .mfa.signals import mfa_disabled, mfa_enabled
-
-@receiver(mfa_disabled)
-def mfa_disabled_callback(sender, **kwargs):
-  recipients = [kwargs['request'].user.email]
-  msg = generate_mfa_disabled_email(recipients)
-  send_email(settings.DEFAULT_FROM_EMAIL, recipients, msg.as_string(), settings.EMAIL_HOST, smtp_server_login=settings.EMAIL_HOST_USER, smtp_server_password=settings.EMAIL_HOST_PASSWORD, proxy_server=settings.PROXY_SERVER, proxy_port=settings.PROXY_PORT)
-
-@receiver(mfa_enabled)
-def mfa_enabled_callback(sender, **kwargs):
-  recipients = [kwargs['request'].user.email]
-  msg = generate_mfa_enabled_email(recipients)
-  send_email(settings.DEFAULT_FROM_EMAIL, recipients, msg.as_string(), settings.EMAIL_HOST, smtp_server_login=settings.EMAIL_HOST_USER, smtp_server_password=settings.EMAIL_HOST_PASSWORD, proxy_server=settings.PROXY_SERVER, proxy_port=settings.PROXY_PORT)
 
 def generate_registration_email(recipients, activation_id):
   subject = settings.USERS_REGISTRATION_EMAIL_SUBJECT
@@ -72,7 +59,7 @@ def generate_password_reset_email(recipients, activation_id):
   msg.attach(part2)
   return msg
 
-def generate_password_change_email(recipients):
+def generate_password_changed_email(recipients):
   subject = settings.USERS_PASSWORD_CHANGE_EMAIL_SUBJECT
   sender = settings.DEFAULT_FROM_EMAIL
   html_template = get_template(settings.USERS_PASSWORD_CHANGE_EMAIL_HTML)
@@ -123,8 +110,14 @@ def generate_mfa_enabled_email(recipients):
   msg.attach(part2)
   return msg
 
-
-def send_email(sender, recipients, message, smtp_server, smtp_server_port=465, smtp_server_login=None, smtp_server_password=None, proxy_server=None, proxy_port=None):
+def send_email(recipients, message,
+  sender=settings.DEFAULT_FROM_EMAIL,
+  smtp_server=settings.EMAIL_HOST,
+  smtp_server_port=465,
+  smtp_server_login=settings.EMAIL_HOST_USER,
+  smtp_server_password=settings.EMAIL_HOST_PASSWORD,
+  proxy_server=settings.PROXY_SERVER,
+  proxy_port=settings.PROXY_PORT):
   if proxy_server:
     import socks
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy_server, proxy_port)
