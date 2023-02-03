e6bb2dec2dabfcbca541deb6205847c1761b0888
dwiest <dw9494@gmail.com>
2023-02-03 23:41:32 +0000

    Moved the generate_mfa_disabled_email and generate_mfa_enabled_email methods to dwiest.django.users.mfa.email.

--- dwiest/django/users/email.py
+++ dwiest/django/users/email.py
@@ -79,39 +79,6 @@ def generate_password_changed_email(recipients):
   msg.attach(part2)
   return msg
 
-def generate_mfa_disabled_email(recipients):
-  subject = settings.USERS_MFA_DISABLED_EMAIL_SUBJECT
-  sender = settings.DEFAULT_FROM_EMAIL
-  html_template = get_template(settings.USERS_MFA_DISABLED_EMAIL_HTML)
-  text_template = get_template(settings.USERS_MFA_DISABLED_EMAIL_TEXT)
-  html_body = html_template.render()
-  text_body = text_template.render()
-  msg = MIMEMultipart('alternative')
-  msg['Subject'] = subject
-  msg['From'] = sender
-  msg['To'] = ', '.join(recipients)
-  part1 = MIMEText(text_body, 'plain')
-  part2 = MIMEText(html_body, 'html')
-  msg.attach(part1)
-  msg.attach(part2)
-  return msg
-
-def generate_mfa_enabled_email(recipients):
-  subject = settings.USERS_MFA_ENABLED_EMAIL_SUBJECT
-  sender = settings.DEFAULT_FROM_EMAIL
-  html_template = get_template(settings.USERS_MFA_ENABLED_EMAIL_HTML)
-  text_template = get_template(settings.USERS_MFA_ENABLED_EMAIL_TEXT)
-  html_body = html_template.render()
-  text_body = text_template.render()
-  msg = MIMEMultipart('alternative')
-  msg['Subject'] = subject
-  msg['From'] = sender
-  msg['To'] = ', '.join(recipients)
-  part1 = MIMEText(text_body, 'plain')
-  part2 = MIMEText(html_body, 'html')
-  msg.attach(part1)
-  msg.attach(part2)
-  return msg
 
 def send_email(recipients, message,
   sender=settings.DEFAULT_FROM_EMAIL,
