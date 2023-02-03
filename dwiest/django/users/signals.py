2de9c31298da6d7148532aa8b191b68ece1bce88
dwiest <dw9494@gmail.com>
2023-02-03 20:38:14 +0000

    User registration and password change/reset signals and handlers.

---
+++ dwiest/django/users/signals.py
@@ -0,0 +1,51 @@
+from django.dispatch import Signal
+from .email import *
+
+user_registration = Signal()
+user_registration_confirmed = Signal()
+resend_registration_email = Signal()
+password_reset_request = Signal()
+password_changed = Signal()
+
+@receiver(user_registration)
+def user_registration_callback(sender, **kwargs):
+  print("user_registration_callback")
+  if getattr(settings, 'EMAIL_SEND', False) == True:
+    recipients = [kwargs['email']]
+    activation_id = kwargs['activation_id']
+    msg = generate_registration_email(recipients, activation_id)
+    send_email(recipients, msg.as_string())
+
+@receiver(user_registration_confirmed)
+def user_registration_confirmed_callback(sender, **kwargs):
+  print("user_registration_confirmed_callback")
+  if getattr(settings, 'EMAIL_SEND', False) == True:
+    recipients = [kwargs['email']]
+    msg = generate_account_activation_email(recipients)
+    send_email(recipients, msg.as_string())
+
+@receiver(resend_registration_email)
+def resend_registration_email_callback(sender, **kwargs):
+  print("resend_registration_email_callback")
+  if getattr(settings, 'EMAIL_SEND', False) == True:
+    recipients = [kwargs['email']]
+    activation_id = kwargs['activation_id']
+    msg = generate_registration_email(recipients, activation_id)
+    send_email(recipients, msg.as_string())
+
+@receiver(password_reset_request)
+def password_reset_callback(sender, **kwargs):
+  print("password_reset_callback")
+  if getattr(settings, 'EMAIL_SEND', False) == True:
+    recipients = [kwargs['email']]
+    activation_id = kwargs['activation_id']
+    msg = generate_password_reset_email(recipients, activation_id)
+    send_email(recipients, msg.as_string())
+
+@receiver(password_changed)
+def password_changed_callback(sender, **kwargs):
+  print("password_changed_callback")
+  if getattr(settings, 'EMAIL_SEND', False) == True:
+    recipients = [kwargs['email']]
+    msg = generate_password_changed_email(recipients)
+    send_email(recipients, msg.as_string())
