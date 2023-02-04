70544af2e78ab631ffe8407c74af128bda6b1a85
dwiest <dw9494@gmail.com>
2023-02-04 00:01:58 +0000

    Moved duplicated code into a _generate_email() function.

--- dwiest/django/users/mfa/email.py
+++ dwiest/django/users/mfa/email.py
@@ -9,31 +9,37 @@ def generate_mfa_disabled_email(recipients):
   sender = settings.DEFAULT_FROM_EMAIL
   html_template = get_template(settings.USERS_MFA_DISABLED_EMAIL_HTML)
   text_template = get_template(settings.USERS_MFA_DISABLED_EMAIL_TEXT)
-  html_body = html_template.render()
-  text_body = text_template.render()
-  msg = MIMEMultipart('alternative')
-  msg['Subject'] = subject
-  msg['From'] = sender
-  msg['To'] = ', '.join(recipients)
-  part1 = MIMEText(text_body, 'plain')
-  part2 = MIMEText(html_body, 'html')
-  msg.attach(part1)
-  msg.attach(part2)
-  return msg
+
+  return _generate_email(
+    sender, recipients, subject, html_template, text_template)
 
 def generate_mfa_enabled_email(recipients):
   subject = settings.USERS_MFA_ENABLED_EMAIL_SUBJECT
   sender = settings.DEFAULT_FROM_EMAIL
   html_template = get_template(settings.USERS_MFA_ENABLED_EMAIL_HTML)
   text_template = get_template(settings.USERS_MFA_ENABLED_EMAIL_TEXT)
-  html_body = html_template.render()
-  text_body = text_template.render()
-  msg = MIMEMultipart('alternative')
+
+  return _generate_email(
+    sender, recipients, subject, html_template, text_template)
+
+def _generate_email( sender, recipients, subject,
+  html_template=None, text_template=None):
+
+  msg = MIMEMultipart('alternative') # need to handle single part
   msg['Subject'] = subject
   msg['From'] = sender
   msg['To'] = ', '.join(recipients)
-  part1 = MIMEText(text_body, 'plain')
-  part2 = MIMEText(html_body, 'html')
-  msg.attach(part1)
-  msg.attach(part2)
+
+  if text_template:
+    text_template = get_template(settings.USERS_MFA_DISABLED_EMAIL_TEXT)
+    text_body = text_template.render()
+    part = MIMEText(text_body, 'plain')
+    msg.attach(part)
+
+  if html_template:
+    html_template = get_template(settings.USERS_MFA_DISABLED_EMAIL_HTML)
+    html_body = html_template.render()
+    part = MIMEText(html_body, 'html')
+    msg.attach(part)
+
   return msg
