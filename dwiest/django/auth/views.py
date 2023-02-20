4419d59b82d276e34bed2d5fa0f01e1e10a15763
dwiest <dw9494@gmail.com>
2023-02-20 00:38:27 +0000

    Removed hardcoded LoginView.template_name, use settings.USERS_LOGIN_TEMPLATE instead.

--- dwiest/django/auth/views.py
+++ dwiest/django/auth/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth import views
 from ..users import views as myViews
 from .forms import AuthenticationForm
 from ..users.forms import PasswordChangeForm
+from ..users.conf import settings
 
 class LoginView(views.LoginView):
   form_class = AuthenticationForm
-  template_name = 'dwiest-django-users/registration/login.html'
+  template_name = settings.USERS_LOGIN_TEMPLATE
 
 class PasswordChangeView(myViews.PasswordChangeView):
   form_class = PasswordChangeForm
