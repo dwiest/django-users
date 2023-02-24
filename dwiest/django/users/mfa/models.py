70961ca0bc9663650c5f75217e02150ef989ae93
dwiest <dw9494@gmail.com>
2023-02-24 22:06:05 +0000

    Replaced hardcoded values with Django settings references.

--- dwiest/django/users/mfa/models.py
+++ dwiest/django/users/mfa/models.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
+from ..conf import settings
 
 class MfaModel(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  secret_key = models.CharField(max_length=32)
-  last_value = models.CharField(max_length=6, null=True, blank=True)
+  user = models.OneToOneField(
+    User,
+    on_delete=models.CASCADE,
+    )
+
+  secret_key = models.CharField(
+    max_length=settings.USERS_MFA_SECRET_KEY_LENGTH,
+    )
+
+  last_value = models.CharField(
+    max_length=settings.USERS_MFA_TOKEN_LENGTH,
+    null=True,
+    blank=True
+    )
